75ec9e265d25c627a83d1012b95e9d908615a06b
James Meggison <jmeggo@hotmail.com>
2015-07-14 18:28:17 +1000

    FInish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
+  config.include Capybara::DSL
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
